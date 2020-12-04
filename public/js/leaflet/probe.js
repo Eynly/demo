9e93d71a81838bf7933b296669b9f9b4512bbcc3
Chang <wbchang@tnsoftware.telenav.com>
2020-12-04 15:25:46 +0800

    add

---
+++ public/js/leaflet/probe.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import App from 'components/app.vue';
+import Routers from './router.js';
+import iView from 'iview';           //引入iview
+import 'iview/dist/styles/iview.css';
